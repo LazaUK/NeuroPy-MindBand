4984b8cea388c119025bb27fe6c13c57bcd2e791
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2019-05-09 19:17:26 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+ 
+setup(name = "NeuroPy",
+      version = "0.x",
+      description = "Library for interfacing with Neurosky's Mindwave EEG headset",
+      author = "sahil singh",
+      author_email = "lihashgnis@yahoo.in",
+      url = "https://github.com/LazaUK/NeuroPy-MindBand",
+      packages=['NeuroPy']
+      )
